95f746d0219921a5272ddd87892e2be7da429f4b
Luiz Fernando <luizfernandoinfo100@gmail.com>
2024-08-17 20:48:38 -0300

    doc: documentando funções

--- src/Filosofo.java
+++ src/Filosofo.java
@@ -1,23 +1,35 @@
-public class Filosofo extends Thread {
-    final static int TEMPO_MAXIMO = 100;
-    Mesa mesa;
-    int filosofo;
+/*Cada filosofo representa um thread em nosso sistema. No java, para criar uma
+nova thread podemos criar uma subclasse da superclasse de mesmo nome.
+* */
 
-    public Filosofo(String nome, Mesa mesadejantar, int fil) {
+public class Filosofo extends Thread {
+    final static int QUANTUM_MAX = 100;
+    Mesa mesa;
+    int pidFilosofo;
+
+    public Filosofo(String nome, Mesa mesadejantar, int pidFilosofo) {
         super(nome);
-        mesa = mesadejantar;
-        filosofo = fil;
+        this.mesa = mesadejantar;
+        this.pidFilosofo = pidFilosofo;
     }
 
+    /*
+    Quando a thread é executada esse método é invocado. Basicamente, ele contem as
+    instruções que a thread deve executar.
+
+    - Chamado pelo método start(), visto que a chamada run() pode causar problemas.
+    */
     public void run() {
-        int tempo = 0;
+        int quantum = 0;
         while (true) {
-            tempo = (int) (Math.random() * TEMPO_MAXIMO);
-            pensar(tempo);
-            mesa.pegarHashis(filosofo);
-            tempo = (int) (Math.random() * TEMPO_MAXIMO);
-            comer(tempo);
-            mesa.retornarHashis(filosofo);
+            quantum = (int) (Math.random() * QUANTUM_MAX);
+            pensar(quantum);
+
+            mesa.pegarHashis(pidFilosofo);
+            quantum = (int) (Math.random() * QUANTUM_MAX);
+
+            comer(quantum);
+            mesa.retornarHashis(pidFilosofo);
         }
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         Mesa mesa = new Mesa ();
-        for (int filosofo = 0; filosofo < 5; filosofo++)
+        for (int pidFilosofo = 0; pidFilosofo < 5; pidFilosofo++)
         {
-            new Filosofo("Filosofo_" + filosofo, mesa, filosofo).start();
+            new Filosofo("Filosofo_" + pidFilosofo, mesa, pidFilosofo).start();
         }
     }
 }
